fdad7b9d5b7cb18fbf39eaf44dfacbb546b2bc43
DESKTOP-NVQVLIB\david <pzach001@ucr.edu>
2019-04-05 08:05:28 -0700

    revert all
    revert all

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -142,9 +142,6 @@ def get_cam_can_parser(CP):
 class CarState(object):
   def __init__(self, CP):
 
-    #self.lkMode = True
-    self.trMode = 1
-    self.read_distance_lines_prev = 1
     self.CP = CP
     self.can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
     self.shifter_values = self.can_define.dv["GEARBOX"]["GEAR_SHIFTER"]
@@ -185,7 +182,7 @@ class CarState(object):
 
     # update prevs, update must run once per loop
     self.prev_cruise_buttons = self.cruise_buttons
-    #self.prev_cruise_setting = self.cruise_setting
+    self.prev_cruise_setting = self.cruise_setting
     self.prev_blinker_on = self.blinker_on
 
     self.prev_left_blinker_on = self.left_blinker_on
@@ -244,7 +241,7 @@ class CarState(object):
     self.angle_steers = cp.vl["STEERING_SENSORS"]['STEER_ANGLE']
     self.angle_steers_rate = cp.vl["STEERING_SENSORS"]['STEER_ANGLE_RATE']
 
-    #self.cruise_setting = cp.vl["SCM_BUTTONS"]['CRUISE_SETTING']
+    self.cruise_setting = cp.vl["SCM_BUTTONS"]['CRUISE_SETTING']
     self.cruise_buttons = cp.vl["SCM_BUTTONS"]['CRUISE_BUTTONS']
 
     self.blinker_on = cp.vl["SCM_FEEDBACK"]['LEFT_BLINKER'] or cp.vl["SCM_FEEDBACK"]['RIGHT_BLINKER']
